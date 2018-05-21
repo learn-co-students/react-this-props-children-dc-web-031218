da9a7946776d04b48797f7d9c21fbe1ad5aa4bff
azeelleeza <leezaluncheon@gmail.com>
2018-05-21 16:28:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-// Code Goes Here
+import React from "react";
+import { render } from "react-dom";
+
+class SomeComponent extends React.Component {
+  render() {
+    const childrenWithWrapperDiv = React.Children.map(
+      this.props.children,
+      child => {
+        return <div className="some-component-special-class">{child}</div>;
+      }
+    );
+
+    return (
+      <div className="some-component">
+        <p>
+          This component has {React.Children.count(this.props.children)}{" "}
+          children.
+        </p>
+        {childrenWithWrapperDiv}
+      </div>
+    );
+  }
+}
+
+const Functional = () => {
+  return (
+    <SomeComponent>
+      <div />
+      <div />
+      <div />
+      <div />
+    </SomeComponent>
+  );
+};
+
+render(<Functional />, document.getElementById("root"));
